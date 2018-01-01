8bd004aecb0af7bb7381fa124d9c6f77663214f6
Anderson Olliver <pla.olliver@gmail.com>
2017-12-31 21:02:08 -0300

    Adicionado termos de uso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './component/home/home.component';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {HomeComponent} from './component/home/home.component';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: '', component: HomeComponent }
+      {path: '', component: HomeComponent},
+      {path: 'termos-de-uso', loadChildren: './pages/termo-de-uso/termo-de-uso.module#TermoDeUsoModule'}
     ])
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MatIconModule, MatIcon, MatToolbarModule, MatNavList, MatSidenavModule, MatListModule, MatDialogModule, MatDialogRef, MatCheckboxModule, 
-          MatFormFieldModule, MatMenuModule, MatInputModule, MatButtonModule, MatCardModule, MatSelectModule, MatTableModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {
+  MatButtonModule, MatCardModule, MatCheckboxModule, MatDatepickerModule, MatDialogModule, MatFormFieldModule, MatIconModule,
+  MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatSelectModule, MatSidenavModule, MatTableModule, MatToolbarModule
+} from '@angular/material';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MediaMatcher } from '@angular/cdk/layout/typings/media-matcher';
-import { FormBuilder } from '@angular/forms/src/form_builder';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HomeComponent } from './component/home/home.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HomeComponent} from './component/home/home.component';
+import {AppRoutingModule} from './app-routing.module';
 import 'hammerjs';
-import { AddLancamentoComponent } from './component/add-lancamento/add-lancamento.component';
-import { LancamentoService } from './service/lancamento.service';
-import { FirebaseConfig } from './../environments/firebase.config';
-import { AngularFireModule } from 'angularfire2/index';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { environment } from '../environments/environment';
-import { MenuUsuarioComponent } from './component/menu-usuario/menu-usuario.component';
+import {AddLancamentoComponent} from './component/add-lancamento/add-lancamento.component';
+import {LancamentoService} from './service/lancamento.service';
+import {FirebaseConfig} from './../environments/firebase.config';
+import {AngularFireModule} from 'angularfire2/index';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {MenuUsuarioComponent} from './component/menu-usuario/menu-usuario.component';
 
 
 @NgModule({
