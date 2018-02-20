d218f2c92a1106e7f642f7e795c4093c1ec99856
unknown <aluno@LAB2D5.ic.ufmt.local>
2018-02-19 20:27:09 -0400

    Atualiza bibliotecas

--- src/app/service/lancamento.service.ts
+++ src/app/service/lancamento.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Lancamento} from '../model/lancamento';
-import {AngularFireDatabase} from 'angularfire2/database';
+// import {AngularFireDatabase} from 'angularfire2/database';
 import {Categoria} from '../model/categoria';
 
 @Injectable()
